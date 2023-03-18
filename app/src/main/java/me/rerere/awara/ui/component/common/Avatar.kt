177f04e00cb1e6ac38d3c90cf6f9b87ceb019d6c
re-ovo <qq1609403959@outlook.com>
2023-03-18 19:37:59 +0800

    index page ui

--- app/src/main/java/me/rerere/awara/ui/component/common/Avatar.kt
+++ app/src/main/java/me/rerere/awara/ui/component/common/Avatar.kt
@@ -1,8 +1,25 @@
 package me.rerere.awara.ui.component.common
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
-fun Avatar() {
-
+fun Avatar(
+    model: String
+) {
+    AsyncImage(
+        model = model,
+        contentDescription = "avatar",
+        modifier = Modifier
+            .clip(CircleShape)
+            .size(32.dp)
+            .background(MaterialTheme.colorScheme.secondaryContainer)
+    )
 }
--- app/src/main/java/me/rerere/awara/ui/page/index/IndexPage.kt
+++ app/src/main/java/me/rerere/awara/ui/page/index/IndexPage.kt
@@ -1,24 +1,41 @@
 package me.rerere.awara.ui.page.index
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Forum
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material.icons.outlined.People
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.VideoLabel
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import me.rerere.awara.R
+import me.rerere.awara.ui.component.common.Avatar
 import me.rerere.awara.ui.hooks.rememberWindowSize
 import org.koin.androidx.compose.koinViewModel
 
@@ -27,69 +44,198 @@ fun IndexPage(
     vm: IndexVM = koinViewModel()
 ) {
     val windowSizeClass = rememberWindowSize()
+    when (windowSizeClass.widthSizeClass) {
+        WindowWidthSizeClass.Expanded -> {
+            IndexPageTabletLayout(vm)
+        }
+
+        WindowWidthSizeClass.Medium, WindowWidthSizeClass.Compact -> {
+            IndexPagePhoneLayout(vm)
+        }
+    }
+}
+
+@Composable
+private fun IndexPageTabletLayout(vm: IndexVM) {
+    val pagerState = rememberPagerState()
+    var query by remember {
+        mutableStateOf("")
+    }
+    var active by remember {
+        mutableStateOf(false)
+    }
     Row {
-        if(windowSizeClass.widthSizeClass == WindowWidthSizeClass.Expanded) {
-            NavigationRail(
-                header = {
-                    Text("Header")
-                }
-            ) {
+        NavigationRail {
+            navigations.forEachIndexed { index, navigationPoint ->
                 NavigationRailItem(
-                    selected = true,
-                    onClick = { /*TODO*/ },
-                    icon = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(Icons.Outlined.Home, contentDescription = "Home")
-                        }
-                    }
-                )
-
-                NavigationRailItem(
-                    selected = false,
-                    onClick = { /*TODO*/ },
-                    icon = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(Icons.Outlined.Home, contentDescription = "Home")
-                        }
-                    }
-                )
-
-                NavigationRailItem(
-                    selected = false,
-                    onClick = { /*TODO*/ },
-                    icon = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(Icons.Outlined.Home, contentDescription = "Home")
-                        }
-                    }
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        // TODO: Handle navigation
+                    },
+                    icon = navigationPoint.icon,
+                    label = navigationPoint.title
                 )
             }
         }
         Scaffold(
             topBar = {
-                TopAppBar(
-                    title = {
-                        Text("Index")
+                SearchBar(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp),
+                    query = query,
+                    onQueryChange = {
+                        query = it
+                    },
+                    onSearch = {
+                    },
+                    active = active,
+                    onActiveChange = { active = it },
+                    leadingIcon = {
+                        Avatar(
+                            model = "https://iwara.tv/images/default-avatar.jpg",
+                        )
+                    },
+                    placeholder = {
+                        Text("今天你想搜点什么?")
+                    },
+                    trailingIcon = {
+                        if (active) {
+                            IconButton(
+                                onClick = { active = false }
+                            ) {
+                                Icon(Icons.Outlined.Close, "Close")
+                            }
+                        } else {
+                            IconButton(
+                                onClick = {
+                                    active = true
+                                }
+                            ) {
+                                Icon(Icons.Outlined.Search, "Search")
+                            }
+                        }
                     }
-                )
-            },
+                ) {}
+            }
         ) { padding ->
-            Column(
-                modifier = Modifier
-                    .padding(padding)
+            HorizontalPager(
+                pageCount = 3,
+                state = pagerState,
+                modifier = Modifier.padding(padding)
             ) {
-                Button(
-                    onClick = {
 
-                    }
-                ) {
-                    Text("Hello World!")
-                }
-
-                Text(
-                    windowSizeClass.toString()
-                )
             }
         }
     }
 }
+
+@Composable
+private fun IndexPagePhoneLayout(vm: IndexVM) {
+    val pagerState = rememberPagerState()
+    var query by remember {
+        mutableStateOf("")
+    }
+    var active by remember {
+        mutableStateOf(false)
+    }
+    Scaffold(
+        topBar = {
+            SearchBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp),
+                query = query,
+                onQueryChange = {
+                    query = it
+                },
+                onSearch = {
+                },
+                active = active,
+                onActiveChange = { active = it },
+                leadingIcon = {
+                    Avatar(
+                        model = "https://iwara.tv/images/default-avatar.jpg",
+                    )
+                },
+                placeholder = {
+                    Text("今天你想搜点什么?")
+                },
+                trailingIcon = {
+                    if (active) {
+                        IconButton(
+                            onClick = { active = false }
+                        ) {
+                            Icon(Icons.Outlined.Close, "Close")
+                        }
+                    } else {
+                        IconButton(
+                            onClick = {
+                                active = true
+                            }
+                        ) {
+                            Icon(Icons.Outlined.Search, "Search")
+                        }
+                    }
+                }
+            ) {
+
+            }
+        },
+        bottomBar = {
+            NavigationBar {
+                navigations.forEachIndexed { index, navigationPoint ->
+                    NavigationBarItem(
+                        selected = pagerState.currentPage == index,
+                        onClick = {
+                            // TODO: Handle navigation
+                        },
+                        icon = navigationPoint.icon,
+                        label = navigationPoint.title
+                    )
+                }
+            }
+        }
+    ) {
+        HorizontalPager(
+            pageCount = 3,
+            state = pagerState,
+            modifier = Modifier.padding(it)
+        ) {
+
+        }
+    }
+}
+
+
+private data class NavigationPoint(
+    val title: @Composable () -> Unit,
+    val icon: @Composable () -> Unit,
+)
+
+private val navigations = listOf(
+    NavigationPoint(
+        title = {
+            Text(stringResource(R.string.index_nav_video))
+        },
+        icon = {
+            Icon(Icons.Outlined.VideoLabel, "Videos")
+        },
+    ),
+    NavigationPoint(
+        title = {
+            Text(stringResource(R.string.index_nav_image))
+        },
+        icon = {
+            Icon(Icons.Outlined.Image, "Images")
+        },
+    ),
+    NavigationPoint(
+        title = {
+            Text(stringResource(R.string.index_nav_video))
+        },
+        icon = {
+            Icon(Icons.Outlined.Forum, "Forum")
+        },
+    ),
+)
