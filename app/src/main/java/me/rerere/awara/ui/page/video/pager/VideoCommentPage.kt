cdf39cd6fa1ef16719dd4f8da45e63d4186b3aef
re-ovo <qq1609403959@outlook.com>
2023-04-11 15:03:49 +0800

    (wip) 评论bottom bar

--- app/src/main/java/me/rerere/awara/ui/page/video/pager/VideoCommentPage.kt
+++ app/src/main/java/me/rerere/awara/ui/page/video/pager/VideoCommentPage.kt
@@ -3,17 +3,27 @@ package me.rerere.awara.ui.page.video.pager
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import me.rerere.awara.ui.component.ext.plus
 import me.rerere.awara.ui.component.iwara.CommentCard
@@ -40,8 +50,27 @@ fun VideoCommentPage(vm: VideoVM) {
             }
         }
 
-        BottomAppBar {
-            Text("???")
+        BottomAppBar(
+            modifier = Modifier.imePadding()
+        ) {
+            TextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text("请友善评论")
+                },
+                modifier = Modifier
+                    .padding(4.dp)
+                    .weight(1f),
+                colors = TextFieldDefaults.textFieldColors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                )
+            )
+
+            FilledTonalIconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Outlined.Send, null)
+            }
         }
     }
 }
