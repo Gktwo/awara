698ad8ebd83a9f04c53ad97f336761eb65b70480
re-ovo <qq1609403959@outlook.com>
2023-04-14 16:23:22 +0800

    update depend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.5"
     }
     packaging {
         resources {
@@ -53,18 +53,18 @@ android {
 
 dependencies {
     // Android KTX
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
 
     // Compose
     // implementation(platform("androidx.compose:compose-bom:2023.01.00"))
-    implementation("androidx.compose.ui:ui:1.4.0-rc01")
-    implementation("androidx.compose.ui:ui-graphics:1.4.0-rc01")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0-rc01")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha08")
-    implementation("androidx.compose.material3:material3-window-size-class:1.1.0-alpha08")
-    implementation("androidx.compose.material:material-icons-extended:1.4.0-rc01")
+    implementation("androidx.compose.ui:ui:1.4.1")
+    implementation("androidx.compose.ui:ui-graphics:1.4.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.1")
+    implementation("androidx.compose.material3:material3:1.1.0-beta02")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.0-beta02")
+    implementation("androidx.compose.material:material-icons-extended:1.4.1")
 
     // Media
     implementation("androidx.media3:media3-exoplayer:1.0.0")
@@ -109,6 +109,9 @@ dependencies {
 
     // Profile Installer
     implementation("androidx.profileinstaller:profileinstaller:1.3.0")
+
+    // Leak Canary
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.10")
 
     // Test
     testImplementation("junit:junit:4.13.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id("com.android.application") version "8.1.0-alpha09" apply false
     id("com.android.library") version "8.1.0-alpha09" apply false
-    kotlin("android") version "1.8.10" apply false
-    kotlin("plugin.serialization") version "1.8.10" apply false
+    kotlin("android") version "1.8.20" apply false
+    kotlin("plugin.serialization") version "1.8.20" apply false
 }
