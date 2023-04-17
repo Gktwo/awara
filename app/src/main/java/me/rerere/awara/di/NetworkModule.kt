e3f1290b08fa0c734b5f6c2760044418f332d36c
re-ovo <qq1609403959@outlook.com>
2023-04-17 15:58:21 +0800

    damn

--- app/src/main/java/me/rerere/awara/di/NetworkModule.kt
+++ app/src/main/java/me/rerere/awara/di/NetworkModule.kt
@@ -1,17 +1,14 @@
 package me.rerere.awara.di
 
+import android.util.Log
 import me.rerere.awara.data.source.HitokotoAPI
 import me.rerere.awara.data.source.IwaraAPI
 import me.rerere.awara.util.SerializationConverterFactory
 import me.rerere.compose_setting.preference.mmkvPreference
-import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
-import okhttp3.ResponseBody
-import okhttp3.ResponseBody.Companion.toResponseBody
-import okhttp3.internal.immutableListOf
+import okhttp3.internal.closeQuietly
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
-import retrofit2.Response
 import retrofit2.Retrofit
 
 private const val TAG = "NetworkModule"
@@ -20,9 +17,7 @@ private const val UA = "Mozilla/5.0 (Linux; Android 12; Pixel 6 Build/SD1A.21081
 val networkModule = module {
     single {
         OkHttpClient.Builder()
-            .connectionSpecs(immutableListOf(
-                ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT
-            ))
+            .connectTimeout(10, java.util.concurrent.TimeUnit.SECONDS)
             .addInterceptor {
                 val request = it.request()
                 val url = request.url
@@ -30,10 +25,6 @@ val networkModule = module {
                     .apply {
                         if(url.host == "api.iwara.tv") {
                             addHeader("User-Agent", UA)
-                            addHeader("Accept", "application/json")
-                            addHeader("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7")
-                            addHeader("Referer", "https://www.iwara.tv")
-                            addHeader("Origin", "https://www.iwara.tv")
 
                             if (url.toString() == "https://api.iwara.tv/user/token") {
                                 if ("refresh_token" in mmkvPreference) {
@@ -79,6 +70,19 @@ val networkModule = module {
 //                    it.proceed(request)
 //                }
 //            }
+            .addInterceptor {
+                var retryCount = 0
+                val request = it.request()
+                var response = it.proceed(request)
+                while (!response.isSuccessful && retryCount < 3) {
+                    Log.w(TAG, "Request is not successful - $retryCount - ${response.code}")
+                    retryCount++
+                    // retry the request
+                    response.closeQuietly()
+                    response = it.proceed(request)
+                }
+                response
+            }
             .build()
     }
 
