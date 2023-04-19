ca596e98f595c6408f55d4b6c292ecfa1c838f72
re-ovo <qq1609403959@outlook.com>
2023-04-19 21:44:33 +0800

    搞不好

--- app/src/main/java/me/rerere/awara/ui/page/user/UserPage.kt
+++ app/src/main/java/me/rerere/awara/ui/page/user/UserPage.kt
@@ -4,27 +4,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import me.rerere.awara.data.entity.toHeaderUrl
 import me.rerere.awara.ui.component.common.BackButton
-import me.rerere.awara.ui.component.common.ImageTopBar
-import me.rerere.awara.ui.component.common.rememberImageTopBarState
 import me.rerere.awara.ui.component.iwara.Avatar
 import org.koin.androidx.compose.koinViewModel
 
@@ -33,34 +29,26 @@ fun UserPage(
     vm: UserVM = koinViewModel()
 ) {
     val profileState = vm.state.profile
-    val topBarState = rememberImageTopBarState()
+    val appBarState = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
         topBar = {
-            ImageTopBar(
-                state = topBarState,
-                image = {
-                    AsyncImage(
-                        model = profileState?.header.toHeaderUrl(),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(16f / 9f),
-                        contentScale = ContentScale.Crop
+            LargeTopAppBar(
+                title = {
+                    Text(
+                        text = profileState?.user?.name ?: "",
                     )
                 },
                 navigationIcon = {
                     BackButton()
                 },
-                title = {
-                    Text(profileState?.user?.name ?: "")
-                }
+                scrollBehavior = appBarState
             )
         }
     ) {
         LazyColumn(
             modifier = Modifier
                 .padding(it)
-                .nestedScroll(topBarState.nestedScrollConnection),
+                .nestedScroll(appBarState.nestedScrollConnection),
             verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
             item {
