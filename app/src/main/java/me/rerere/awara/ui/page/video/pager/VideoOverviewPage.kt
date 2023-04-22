c3a6f9723d2ce032288efe14e7c88100db2e72a1
re-ovo <qq1609403959@outlook.com>
2023-04-22 21:06:41 +0800

    fix: 修复状态栏颜色在视频页面直接切换后无法恢复的问题

--- app/src/main/java/me/rerere/awara/ui/page/video/pager/VideoOverviewPage.kt
+++ app/src/main/java/me/rerere/awara/ui/page/video/pager/VideoOverviewPage.kt
@@ -3,41 +3,41 @@ package me.rerere.awara.ui.page.video.pager
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Download
-import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material.icons.outlined.PlaylistAdd
 import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material.icons.outlined.Translate
-import androidx.compose.material3.Card
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -95,7 +95,7 @@ fun VideoOverviewPage(vm: VideoVM) {
 private fun VideoInfoCard(video: Video, vm: VideoVM) {
     val (expand, setExpand) = remember { mutableStateOf(false) }
     val context = LocalContext.current
-    Card(
+    Box(
         modifier = Modifier
             .padding(horizontal = 8.dp)
             .animateContentSize()
@@ -105,36 +105,40 @@ private fun VideoInfoCard(video: Video, vm: VideoVM) {
             verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier.padding(8.dp),
         ) {
-            Row {
-                Column(
-                    modifier = Modifier.weight(1f),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(IntrinsicSize.Min),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
                     Text(
                         text = video.title,
                         style = MaterialTheme.typography.titleLarge,
-                        maxLines = 1
+                        maxLines = 1,
+                        modifier = Modifier.weight(1f)
                     )
-
-                    if (!video.body.isNullOrBlank()) {
-                        RichText(
-                            text = video.body.trim(),
-                            style = MaterialTheme.typography.bodySmall,
-                            maxLines = if (expand) Int.MAX_VALUE else 2,
-                            overflow = TextOverflow.Ellipsis,
-                            onLinkClick = {
-                                context.openUrl(it)
-                            }
-                        )
-                    }
-                }
-
-                IconButton(
-                    onClick = { setExpand(!expand) }
-                ) {
                     Icon(
                         imageVector = if (expand) Icons.Outlined.KeyboardArrowUp else Icons.Outlined.KeyboardArrowDown,
-                        contentDescription = "More"
+                        contentDescription = "More",
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .fillMaxHeight()
+                            .aspectRatio(1f)
+                            .clickable {
+                                setExpand(!expand)
+                            }
+                    )
+                }
+
+                if (!video.body.isNullOrBlank() && expand) {
+                    RichText(
+                        text = video.body.trim(),
+                        style = MaterialTheme.typography.bodySmall,
+                        overflow = TextOverflow.Ellipsis,
+                        onLinkClick = {
+                            context.openUrl(it)
+                        }
                     )
                 }
             }
@@ -196,7 +200,7 @@ private fun VideoInfoCard(video: Video, vm: VideoVM) {
 @Composable
 private fun AuthorCard(video: Video) {
     val router = LocalRouterProvider.current
-    Card(
+    OutlinedCard(
         modifier = Modifier
             .padding(horizontal = 8.dp)
             .fillMaxWidth(),
