679f83268e7d12696257e8704f7f2a31bedf3dc9
re-ovo <qq1609403959@outlook.com>
2023-04-28 23:57:37 +0800

    (wip) search page

--- app/src/main/java/me/rerere/awara/service/DownloadService.kt
+++ app/src/main/java/me/rerere/awara/service/DownloadService.kt
@@ -8,19 +8,6 @@ import android.os.IBinder
 private const val TAG = "DownloadService"
 
 class DownloadService : Service() {
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        intent?.let { startIntent ->
-            val url = startIntent.getStringExtra("url")
-            val name = startIntent.getStringExtra("name")
-            val type = startIntent.getStringExtra("type")
-            // TODO: Download
-        }
-        return super.onStartCommand(intent, flags, startId)
-    }
-
     override fun onBind(intent: Intent?): IBinder = DownloadBinder()
-
-    inner class DownloadBinder : Binder() {
-
-    }
+    inner class DownloadBinder : Binder()
 }
--- app/src/main/java/me/rerere/awara/ui/page/search/SearchPage.kt
+++ app/src/main/java/me/rerere/awara/ui/page/search/SearchPage.kt
@@ -1,13 +1,20 @@
 package me.rerere.awara.ui.page.search
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import me.rerere.awara.R
 import me.rerere.awara.ui.component.common.BackButton
+import me.rerere.awara.ui.component.common.Spin
+import me.rerere.awara.ui.component.iwara.PaginationBar
 
 @Composable
 fun SearchPage() {
@@ -15,16 +22,27 @@ fun SearchPage() {
         topBar = {
             TopAppBar(
                 title = {
-                    Text("Search")
+                    Text(stringResource(R.string.search))
                 },
                 navigationIcon = {
                     BackButton()
                 }
             )
+        },
+        bottomBar = {
+            PaginationBar(
+                page = 1,
+                limit = 32,
+                total = 0,
+                onPageChange = {},
+                contentPadding = WindowInsets.navigationBars.asPaddingValues()
+            )
         }
     ) {
-        Column(
+        Spin(
+            show = true,
             modifier = Modifier
+                .fillMaxSize()
                 .padding(it)
         ) {
 
